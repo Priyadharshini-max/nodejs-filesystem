1f7dc52b05c44cbe9d5eba56107dbe61216c51be
Priya R <priyareddyece97@gmail.com>
2021-10-13 13:01:55 +0530

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var createfileRoute = require("./Routes/Createfile");
 var displayfilesRoute = require("./Routes/Displayfiles");
 
-app.listen("3001", () => {
+app.listen(3001, () => {
     console.log("Connected to server...");
 })
 
