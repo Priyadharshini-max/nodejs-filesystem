fe3084e29544db4b74d32ddf7710a4f8d2c7d12c
Priya R <priyareddyece97@gmail.com>
2021-10-13 13:28:46 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
+const cors = require("cors");
 var createfileRoute = require("./Routes/Createfile");
 var displayfilesRoute = require("./Routes/Displayfiles");
 
-app.listen(3001, () => {
+var PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log("Connected to server...");
 })
 
+app.use(cors());
 app.use("/createfile", createfileRoute);
 app.use("/displayfiles", displayfilesRoute);
 
